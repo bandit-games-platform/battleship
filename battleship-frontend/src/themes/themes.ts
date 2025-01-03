0d4fa049d0104acf8349585c209a779a5fb4ed01
boldi-kdg <boldizsar.olajos@student.kdg.be>
2025-01-03 11:39:42 +0100

    Arranging ships

--- battleship-frontend/src/themes/themes.ts
+++ battleship-frontend/src/themes/themes.ts
@@ -10,7 +10,16 @@ export const themes: BattleshipTheme[] = [
         board_background_anim: {
             src: assets + "concept/water_animated.png",
             cols: 4,
-            rows: 2
+            rows: 2,
+            speed: 0.1
+        },
+        board_grid: {line_width: 3, line_color: 0xdddddd, line_alpha: 0.3},
+        ships: {
+            CARRIER: assets + "concept/carrier.png",
+            BATTLESHIP: assets + "concept/battleship.png",
+            CRUISER: assets + "concept/cruiser.png",
+            SUBMARINE: assets + "concept/submarine.png",
+            DESTROYER: assets + "concept/destroyer.png",
         },
         effects: {
             explosion_anim: {
