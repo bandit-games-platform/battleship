cc9cc965810ce4f71e56bbaef26d9822a275a782
Roman Gordon <roman.gordon@student.kdg.be>
2025-01-08 17:00:24 +0100

    Hopefully complete setup for cicd

--- battleship-backend/build.gradle.kts
+++ battleship-backend/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.gorylenko.gradle-git-properties") version "2.4.1"
 }
 
 group = "be.kdg.int5"
@@ -27,8 +28,54 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//Spring cloud AZURE
+	implementation("com.azure.spring:spring-cloud-azure-starter-actuator:4.3.0")
+	implementation("com.azure.spring:spring-cloud-azure-starter-keyvault:4.3.0")
+	implementation("com.azure.spring:spring-cloud-azure-starter:4.3.0")
+	implementation("com.azure.spring:spring-cloud-azure-dependencies:5.7.0")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.named("generateGitProperties") {
+	outputs.upToDateWhen { false }
+}
+
+tasks.register("printGitProperties") {
+	dependsOn("generateGitProperties")
+	doLast {
+		val gitProps = project.extra["gitProps"] as Map<String, String>
+		println("git.commit.id.abbrev=${gitProps["git.commit.id.abbrev"]}")
+	}
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootBuildImage>("bootBuildImage") {
+	builder.set("paketobuildpacks/builder-jammy-base:latest")
+	imageName.set("acrbattleshipgame.azurecr.io/backend")
+
+	val gitProps = project.extra["gitProps"]
+	val abbrevTag = if (gitProps is Map<*, *>) {
+		gitProps["git.commit.id.abbrev"]?.toString() ?: "unknown"
+	} else {
+		"unknown"
+	}
+
+	tags.set(
+		listOf("acrbattleshipgame.azurecr.io/backend:$abbrevTag")
+	)
+	publish.set(true)
+	docker {
+		publishRegistry {
+			username.set(System.getenv("GAME_REGISTRY_USERNAME"))
+			password.set(System.getenv("GAME_REGISTRY_PASSWORD"))
+		}
+	}
+	archiveFile.set(tasks.named("bootJar").get().outputs.files.singleFile)
+}
+
+tasks.named("bootBuildImage") {
+	dependsOn("generateGitProperties")
 }
