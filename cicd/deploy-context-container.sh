fa69201888c277da11f31a868fd4170ef87220e8
Roman Gordon <roman.gordon@student.kdg.be>
2025-01-09 23:33:43 +0100

    Reverted changes, turns out container was still deploying

--- cicd/deploy-context-container.sh
+++ cicd/deploy-context-container.sh
@@ -93,10 +93,3 @@ fi
 
 unset ENV_VAR_STRING
 
-az containerapp ingress cors enable \
-  --name $CONTAINER_NAME \
-  --resource-group $RESOURCE_GROUP \
-  --allowed-origins "$FRONTEND_URL" \
-  --allowed-methods "*" \
-  --allowed-headers Content-Type Authorization \
-  --allow-credentials true
